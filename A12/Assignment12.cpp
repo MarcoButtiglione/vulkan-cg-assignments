c7603105ca27b93c629bb8428115cf6a03c0cb1d
Marco Buttiglione <marco.buttiglione98@gmail.com>
2022-04-26 09:49:49 +0200

    A12 fix

--- A12/Assignment12.cpp
+++ A12/Assignment12.cpp
@@ -60,7 +60,7 @@ private:
     void createInstance() {
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.pApplicationName = "Assignment 12";
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName = "No Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
@@ -69,6 +69,20 @@ private:
         VkInstanceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
+
+        //List of global extensions
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount,
+            extensions.data());
+
+        std::cout << "Available global extensions:\n";
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+        //
+
 
         uint32_t glfwExtensionCount = 0;
         const char** glfwExtensions;
